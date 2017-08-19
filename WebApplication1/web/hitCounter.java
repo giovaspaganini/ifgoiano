8fe9f458099b1dd8a2e097a8597b4ada7aab7eaf
giovaspaganini <giovanipaganini30@hotmail.com>
2017-08-18 21:12:16 -0300

    nem sei o que alterei

--- WebApplication1/web/hitCounter.java
+++ WebApplication1/web/hitCounter.java
@@ -32,8 +32,8 @@ public class PageHitCounter extends HttpServlet {
             "<body bgcolor = \"#f0f0f0\">\n" +
                "<h1 align = \"center\">" + title + "</h1>\n" +
                "<h2 align = \"center\">" + hitCount + "</h2>\n" +
-            "</body>
-         </html>"
+            "</body>"
+              + "</html>"
       );
    }
    
