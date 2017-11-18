b9310a34fc9b6e3659275def3b041e57a157cdad
Giovani Paganini <giovanipaganini@outlook.com>
2017-11-18 15:47:22 -0200

    folder realocation

---
+++ DCW/av-1-2/types.js
@@ -0,0 +1,28 @@
+var x = 5;
+var y = 4;
+
+var z = x + y;
+
+var t = String(z); //converte o z para string utilizando um metodo com parametro;
+
+z.toString(); //converte para string com a função toString() sem receber parametro;
+
+var k = Number(t);
+
+t.parseFloat();
+
+///classe carros;
+///carro fusca = objeto;
+
+///atributos
+//cor
+//modelo
+//placa
+
+
+///metodos
+
+//freiar();
+//acelerar();
+//ligar();
+//autodestruicao();
--- DCW/types.js
+++
@@ -1,6 +0,0 @@
-var x = 5;
-var y = 4;
-
-var z = x + y;
-
-console.log(z);
