48ba68b2ac048bd592f7f424943c9117aa233d53
Giovani Paganini <giovanipaganini@outlook.com>
2017-11-18 18:43:55 -0200

    addPessoa function add

---
+++ DCW/av-3/js/ImcCalc.js
@@ -0,0 +1,35 @@
+var nome = new Array();
+var peso = new Array();
+var altura = new Array();
+var imc = new Array();
+var imcResult = new Array();
+var i = new Array();
+var aux = 0;
+
+function addPessoa(){
+    do{
+        nome[aux] = prompt("Seu nome: ");
+    } while (nome[aux] == "" || nome[aux] == null || ValidaNome(nome[aux]) == false )
+    
+    do {
+        peso[aux] = prompt("Seu peso: ");
+    } while (peso[aux] == "" || peso[aux] == null || ValidaPeso(peso[aux]) == false)
+    
+    do {
+        altura[aux] = prompt("Sua altura:");
+    } while (altura[aux] == "" || altura[aux] == null || ValidaAltura(altura[aux]) == false)
+
+    var se = prompt("Digite: 1 - Novo | 2 - Calcular IMC");
+
+    if (se == 1){
+        AddPessoa();
+
+    }else if(se == 2){
+        for (var a = 0; a <= aux; a++ ) {
+            i[a] = ("Nome: " + nome[a] +
+             "\n IMC: " + ImcCalc(peso[a], altura[a]).toFixed(2) +
+              "\n Nível: " + ImcResult(ImcCalc(peso[a], altura[a]))+ "\n\n");
+        }
+        alert(i.toString());
+    }
+}
